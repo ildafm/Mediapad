71e205b1d3e44e70930a470ab3269bf9f96bd61f
TPWS11 <tangguhpws12@mhs.mdp.ac.id>
2022-06-05 17:53:12 +0700

    <RequestPermission>

--- app/src/main/java/com/kelompok06_RPL/mediapad/MainActivity.java
+++ app/src/main/java/com/kelompok06_RPL/mediapad/MainActivity.java
@@ -10,30 +10,64 @@ Sthepanie
 Tangguh Prana Wela Sukma
  */
 
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
-import android.widget.Switch;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bnvNavigationView;
     private FrameLayout flContainer;
 
+    ActivityResultLauncher<String[]> PermissionResultLauncher;
+    private boolean readPermissionGanted = false;
+    private boolean locationPermissionGranted = false;
+    private boolean recordPermissionGranted = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PermissionResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), new ActivityResultCallback<Map<String, Boolean>>() {
+            @Override
+            public void onActivityResult(Map<String, Boolean> result) {
+                if (result.get(Manifest.permission.READ_EXTERNAL_STORAGE)!= null){
+                    readPermissionGanted = result.get(Manifest.permission.READ_EXTERNAL_STORAGE);
+                }
+                if (result.get(Manifest.permission.ACCESS_FINE_LOCATION)!= null){
+                    locationPermissionGranted = result.get(Manifest.permission.ACCESS_FINE_LOCATION);
+                }
+                if (result.get(Manifest.permission.RECORD_AUDIO)!= null){
+                    recordPermissionGranted = result.get(Manifest.permission.RECORD_AUDIO);
+                }
+            }
+        });
+
+        requestPermission();
 
         bukaFragment(new VideoFragment());
         getSupportActionBar().setTitle("Video");
@@ -70,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
     }
 
     private void bukaFragment(Fragment FRG){
@@ -83,6 +118,36 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_top, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void requestPermission(){
+        readPermissionGanted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED;
+        locationPermissionGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED;
+        recordPermissionGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+        ) == PackageManager.PERMISSION_GRANTED;
+
+        List<String> permissionRequest = new ArrayList<String>();
+        if (!readPermissionGanted){
+            permissionRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        if (!locationPermissionGranted){
+            permissionRequest.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (!recordPermissionGranted){
+            permissionRequest.add(Manifest.permission.RECORD_AUDIO);
+        }
+
+        if (!permissionRequest.isEmpty()){
+            PermissionResultLauncher.launch(permissionRequest.toArray(new String[0]));
+        }
     }
 
 }
