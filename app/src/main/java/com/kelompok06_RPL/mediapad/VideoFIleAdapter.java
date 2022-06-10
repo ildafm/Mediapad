f2ee81dc94a37d0213de0b732fa25bcdfd3625cb
TPWS11 <tangguhpws12@mhs.mdp.ac.id>
2022-06-10 18:22:47 +0700

    <get Video

---
+++ app/src/main/java/com/kelompok06_RPL/mediapad/VideoFIleAdapter.java
@@ -0,0 +1,101 @@
+package com.kelompok06_RPL.mediapad;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class VideoFIleAdapter extends RecyclerView.Adapter<VideoFIleAdapter.ViewHolder> {
+    private ArrayList<FileMedia> videoList;
+    private Context context;
+
+    public VideoFIleAdapter(ArrayList<FileMedia> videoList, Context context) {
+        this.videoList = videoList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.video_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.vidName.setText(videoList.get(position).getDisplayName());
+        String size = videoList.get(position).getSize();
+        holder.vidSize.setText(android.text.format.Formatter.formatFileSize(context,
+                Long.parseLong(size)));
+        double millisecond = Double.parseDouble(videoList.get(position).getDuration());
+        holder.vidDur.setText(timeConversion((long) millisecond));
+//        holder.vidDur.setText((CharSequence) new File(videoList.get(position).getDuration()));
+        Glide.with(context)
+                .load(new File(videoList.get(position).getPath()))
+                .into(holder.thumbnail);
+
+//        Glide.with(context)
+//                .load(Uri.fromFile(new File(videoList.get(position).getPath())))
+//                .thumbnail(0.1f)
+//                .into(holder.thumbnail);
+        holder.menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "menu more", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, VideoPlayerActivity.class);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return videoList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView thumbnail, menu;
+        TextView vidName, vidSize, vidDur;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            thumbnail = itemView.findViewById(R.id.iv_thumb);
+            menu = itemView.findViewById(R.id.vid_menu_more);
+            vidName = itemView.findViewById(R.id.vid_name);
+            vidSize = itemView.findViewById(R.id.video_size);
+            vidDur = itemView.findViewById(R.id.tv_duration);
+        }
+    }
+
+    public String timeConversion(long value) {
+        String videoTime;
+        int duration = (int) value;
+        int hrs = (duration / 3600000);
+        int mns = (duration / 60000) % 60000;
+        int scs = duration % 60000 / 1000;
+        if (hrs > 0) {
+            videoTime = String.format("%02d:%02d:%02d", hrs, mns, scs);
+        } else {
+            videoTime = String.format("%02d:%02d", mns, scs);
+        }
+        return videoTime;
+    }
+}
--- app/src/main/java/com/kelompok06_RPL/mediapad/VideoFileActivity.java
+++ app/src/main/java/com/kelompok06_RPL/mediapad/VideoFileActivity.java
@@ -1,14 +1,64 @@
 package com.kelompok06_RPL.mediapad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
 
 public class VideoFileActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    private ArrayList<FileMedia> videoFileArrayList = new ArrayList<>();
+    VideoFIleAdapter videoFIleAdapter;
+    String folder_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_file);
+        folder_name = getIntent().getStringExtra("folderName");
+        getSupportActionBar().setTitle(folder_name);
+        recyclerView = findViewById(R.id.rv_video);
+
+        showVideoFiles();
+    }
+
+    private void showVideoFiles() {
+        videoFileArrayList = fetchMedia(folder_name);
+        videoFIleAdapter = new VideoFIleAdapter(videoFileArrayList, this);
+        recyclerView.setAdapter(videoFIleAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                RecyclerView.VERTICAL, false));
+        videoFIleAdapter.notifyDataSetChanged();
+    }
+
+    private ArrayList<FileMedia> fetchMedia(String folderName) {
+        ArrayList<FileMedia> videoFile = new ArrayList<>();
+        Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Video.Media.DATA + " like?";
+        String[] selectionArg = new String[]{"%" + folderName + "%"};
+        Cursor cursor = getContentResolver().query(uri, null,
+                selection, selectionArg, null);
+        if (cursor != null && cursor.moveToNext()) {
+            do {
+                @SuppressLint("Range") String id = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media._ID));
+                @SuppressLint("Range") String title = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.TITLE));
+                @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                @SuppressLint("Range") String size = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                @SuppressLint("Range") String duration = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                @SuppressLint("Range") String path = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                @SuppressLint("Range") String date = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATE_ADDED));
+
+                FileMedia fileMedia = new FileMedia(id, title, name, size, duration, path, date);
+                videoFile.add(fileMedia);
+            } while (cursor.moveToNext());
+        }
+        return videoFile;
     }
 }
