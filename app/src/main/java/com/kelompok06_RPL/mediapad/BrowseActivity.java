59f76e6c4d3617d85dd87ba285fb130cf3262c28
TPWS11 <tangguhpws12@mhs.mdp.ac.id>
2022-06-13 00:15:49 +0700

    <Update Browse

--- app/src/main/java/com/kelompok06_RPL/mediapad/BrowseActivity.java
+++ app/src/main/java/com/kelompok06_RPL/mediapad/BrowseActivity.java
@@ -7,18 +7,35 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class BrowseActivity extends AppCompatActivity {
     private BottomNavigationView bnvNavigationView;
+    ImageView video,music;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
         bnvNavigationView = findViewById(R.id.bnv_navigasi_bottom);
         bnvNavigationView.setSelectedItemId(R.id.menu_browse);
+        video = findViewById(R.id.folder_vid);
+        music = findViewById(R.id.folder_msc);
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+        music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MusikActivity.class));
+            }
+        });
 
         bnvNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
