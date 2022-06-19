7dff4d3aecbb20ba218da6ed4fd20f42ea4bc3b4
TPWS11 <tangguhpws12@mhs.mdp.ac.id>
2022-06-19 15:10:27 +0700

    <update

--- app/src/main/java/com/kelompok06_RPL/mediapad/BrowseActivity.java
+++ app/src/main/java/com/kelompok06_RPL/mediapad/BrowseActivity.java
@@ -38,7 +38,7 @@ public class BrowseActivity extends AppCompatActivity {
         music.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MusikActivity.class));
+                startActivity(new Intent(getApplicationContext(),MscActivity.class));
             }
         });
 
